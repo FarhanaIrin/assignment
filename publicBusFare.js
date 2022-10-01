4166b43b278666ca3b6fd827e3fddf5f2daa8761
FarhanaIrin <111515775+FarhanaIrin@users.noreply.github.com>
2022-10-01 22:04:46 +0600

    Delete publicBusFare.js

--- publicBusFare.js
+++
@@ -1,8 +0,0 @@
-function publicBusFare(passenger){
-    let bus= passenger / 50;
-    let fileExtension = bus.split('.').pop();
-   // let microbus = passenger - bus*50;
-    console.log(fileExtension);
-}
-
-publicBusFare(117);
